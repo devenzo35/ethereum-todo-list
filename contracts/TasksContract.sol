3343412e3ff756cc69683f256af30497327b9505
Enzo <enzocuellar12@gmail.com>
2021-09-28 14:53:54 -0300

    contract method to create tasks

--- contracts/TasksContract.sol
+++ contracts/TasksContract.sol
@@ -2,13 +2,28 @@
 pragma solidity ^0.8.6;
 
 contract TasksContract {
+    uint256 counter = 0;
+
     struct Task {
         uint256 id;
         string name;
         string description;
-        uint256 createdAt;
         bool done;
+        uint256 createdAt;
     }
 
     mapping(uint256 => Task) public tasks;
+
+    function createTask(string memory _title, string memory _description)
+        public
+    {
+        tasks[counter] = Task(
+            counter,
+            _title,
+            _description,
+            false,
+            block.timestamp
+        );
+        counter++;
+    }
 }
